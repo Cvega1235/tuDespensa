16a0b1eade0777c12a70475ea8490dd3a13a55d1
GabrielCam123 <cameotapia@gmail.com>
2025-03-25 20:26:28 -0400

    Pruebahost

--- TuDespensa-Web/src/api/axios.js
+++ TuDespensa-Web/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:4000/api",
-  // baseURL: "backend-production-e141.up.railway.app",
+  // baseURL: "http://localhost:4000/api",
+  baseURL: "backend-production-e141.up.railway.app",
   withCredentials: true,
 });
 export default instance;
--- backend2/src/app.js
+++ backend2/src/app.js
@@ -1,23 +1,23 @@
-import express from 'express';
-import morgan from 'morgan';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import express from "express";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
-import tasksRoutes from './routes/tasks.routes.js'
+import tasksRoutes from "./routes/tasks.routes.js";
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-    cors({
-      // origin: "http://localhost:5173", 
-      origin:"frontend-production-29e6.up.railway.app",
-      credentials: true, 
-    })
-  );
+  cors({
+    // origin: "http://localhost:5173",
+    origin: "frontend-production-29e6.up.railway.app",
+    credentials: true,
+  })
+);
 
 app.use("/api", authRoutes);
 
